32c2c2c1516b81b0becce58cc05d37a560146e4e
OleHallvard <ole_hallvard@hotmail.com>
2020-02-11 00:09:01 +0100

    Init commit

---
+++ index.php
@@ -0,0 +1,658 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-134318014-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-134318014-1');
+</script>
+
+
+
+  <meta charset="UTF-8">
+  <title>Ole Hallvard</title>
+  <link rel="stylesheet" href="/css/style.css">
+  <link rel="stylesheet" href="/css/deltstyle.css">
+  
+
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:100,300,300i,400,600,700" rel="stylesheet">
+  <script src="js/smoothScroll.js"></script>
+<meta name="viewport" content="width=device-width">
+<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+
+</head>
+
+
+
+<style>
+::-moz-selection { /* Code for Firefox */
+    color: #eee;
+    background: #1b14ac;
+}
+
+::selection {
+    color: #eee;
+    background:  #1b14ac;
+}
+
+
+</style>
+
+
+<body>
+
+
+<?php include("includes/meny.php"); ?>
+
+
+
+<div class="tiltoppen"><a id="top"></a></div>
+
+
+
+
+
+
+
+<div class="velkommen">
+
+	<!--<div class="fargeeffekt"></div>-->
+
+	
+
+
+
+	<div class="tekstblokk">
+
+
+
+			<h1><span>Ole<br></span></h1>
+			<h1><span>Hallvard<br></span></h1>			
+			<h1><span>Kveseth</span></h1>
+			<h6><span>Art Director</span></h6>
+			
+			
+			<!-- <h2>Kreatør &nbsp<span class="pln"> I </span> &nbsp Art Director</h2> --> ​
+			
+
+<!--
+
+			<div class="Iam">
+
+			<b>
+  			<div class="innerIam">
+    			Art Director &nbsp <span class="pln"> I </span>  &nbsp Designer </span><br /> 
+    			Reklame &nbsp <span class="pln"> I </span>  &nbsp Web </span><br /> 
+    			Animasjon &nbsp <span class="pln"> I </span>  &nbsp foto </span><br />
+    			
+    			</div>
+			</b>
+
+			</div>
+		-->
+	
+			<p class="desktoptekst"><span>	
+
+			 <!--Jeg liker å finne på ideer. Ideer som kommuniserer noe til noen – og som fører til en handling. Deretter produsere ideene gjennom design, film, animasjon, foto og kode. -->
+			
+			</span></p>
+
+			<p class="mobiltekst"><span>
+
+			 <!--Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.	-->
+			</span></p>
+			
+			
+			
+			
+			<a class="btn2" a href="#tilprosjekterdelen" onclick="smoothScroll('tilprosjekterdelen')">
+    	    	<span class="row1"><h2>Mine arbeider</h2></span>
+	        	<span class="row3"><h2>Se</h2></span>
+      		
+      		
+
+      		<a class="btn" href="ommeg">
+    	    	<span class="row1"><h2>Om meg</h2></span>
+	        	<span class="row2"><h2>Les</h2></span>
+      		</a>
+
+      		<p id="safari"> ! For å spille av filmen i bakgrunnen, bytt til Chrome/Firefox for en bedre opplevelse av siden :-)</p>
+      		
+	</div>	
+
+
+	<div class="fullscreen-bg">
+    <video muted playsinline loop autoplay  poster="img/Main/OH-showreel-poster.png" class="fullscreen-bg__video">
+        <source src="img/Main/OH_showreel_stor.mp4" type="video/webm">
+    </video>
+</div>
+
+
+	<div class="bakgrunnsfoto"></div>
+		
+	</div>
+
+
+
+
+
+
+
+
+	
+	<div class="ikoner">
+
+
+		<a href="https://www.facebook.com/ole.kveseth" target="_blank" 
+		div class="fbcircle">
+			<img class="facebook" src="img/Main/Facebook.svg"/></a>
+		
+		
+		<a href="https://www.linkedin.com/in/ole-hallvard-kveseth-97706744/" target="_blank" 
+		div class="licircle">
+			<img class="linkdin" src="img/Main/linkedin-icon.png"/></a>
+		
+		
+
+		<div class="scroll-downs">
+  			<div class="mousey">
+    			<div class="scroller"></div>
+  			</div>
+		</div>
+		
+		<div class="scrolltekst">Scroll</div>
+		
+		
+	</div>
+		
+
+	
+
+
+
+	
+
+	<!--
+	<video poster="img/Poster_film.jpg" playsinline autoplay muted loop>
+		<source src="img/Bakgrunnsfilm.mp4"" type="video/mp4">
+		</video>
+	-->
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="prosjektwrapper">
+
+	<div class="tilprosjekter"><a id="tilprosjekterdelen"></a></div>
+
+
+
+
+
+
+
+	<a href="/prosjekter/imdi"><div class="prosjekt2">
+
+		<div class="tekstwrapper">
+
+						<h4>IMDI</h4>
+
+						<h5>Motivasjonsfilm</h5>
+			
+			
+				</div>
+
+			<div class="sortoverlay"></div>
+			<img id="thumb" src="../img/imdi/imdi_toppbilde.jpg" alt="IMDi film">
+		
+	</a>
+	</div>
+
+
+
+
+
+	<a href="/prosjekter/113appen"><div class="prosjekt2">
+
+		<div class="tekstwrapper">
+
+						<h4>SNLA</h4>
+
+						<h5>113 Appen</h5>
+			
+			
+				</div>
+
+			<div class="sortoverlay"></div>
+			<img id="thumb" src="../img/Snla/snla_thumb_love.png" alt="113 appen">
+		
+	</a>
+	</div>
+
+
+
+
+
+	<a href="/prosjekter/Flytoget2"><div class="prosjekt2">
+
+		<div class="tekstwrapper">
+
+						<h4>Flytoget</h4>
+
+						<h5>Kampanjer</h5>
+			
+			
+				</div>
+
+			<div class="sortoverlay"></div>
+			<img id="thumb" src="../img/Flytoget/flytoget.jpg" alt="Flytogetbilde">
+		
+	</a>
+	</div>
+
+
+
+
+
+
+
+	<a href="/prosjekter/Norwegian"><div class="prosjekt2">
+
+		<div class="tekstwrapper">
+
+						<h4>Norwegian</h4>
+				
+						<h5>Kampanjer</h5>
+			
+				</div>
+			<div class="sortoverlay"></div>
+			<img id="thumb" src="../img/Norwegian/Norwegian.jpg" alt="Norwegianbilde">
+	</a>
+	</div>
+
+
+
+
+
+
+
+
+
+
+	<a href="/prosjekter/Statensvegvesen"><div class="prosjekt2">
+
+				<div class="tekstwrapper">
+
+						<h4>Statens Vegvesen</h4>
+				
+						<h5>Fast & Safe</h5>
+			
+				</div>
+
+			<div class="sortoverlay"></div>
+				<img id="thumb" src="../img/Statensvegvesen/Fast&safe2.jpg" alt="Bilde fra Fast & Safe">
+		
+			</a>
+	</div>
+
+
+
+
+
+
+
+
+
+
+
+	<a href="/prosjekter/Smartidrettsmat"><div class="prosjekt2">
+
+		<div class="tekstwrapper">
+
+			
+						<h4>Tine</h4>
+					
+						<h5>Smart Idrettsmat</h5>
+					
+			
+				</div>
+			<div class="sortoverlay"></div>
+				<img id="thumb" src="../img/Smartidrettsmat/TineSI_Mockup_Film_Planlegger_thumb.jpg" alt="Bilde fra Smart Idrettsmat">
+	</a>
+	</div>
+
+
+
+
+
+
+
+
+
+
+
+	<a href="/prosjekter/Bouvetgrandprix.php"><div class="prosjekt2">
+
+		<div class="tekstwrapper">
+
+			
+						<h4>Bouvet</h4>
+					
+						<h5>Bouvet Grand Prix</h5>
+					
+			
+				</div>
+			<div class="sortoverlay"></div>
+			<img id="thumb" src="../img/Bouvetgrandprix/bgp.jpg" alt="Bilde fra Bouvet Grand Prix">
+			
+	</a>
+	</div>
+
+
+
+
+
+
+
+
+
+	<a href="/prosjekter/Ruter"><div class="prosjekt2">
+
+		<div class="tekstwrapper">
+
+						<h4>Ruter</h4>
+
+						<h5>Kampanjer</h5>
+
+			
+				</div>
+			<div class="sortoverlay"></div>
+			<img id="thumb" src="../img/Ruter/Ruter2.jpg" alt="Bilde fra Ruter">
+			
+		</a>
+	</div>
+
+
+
+
+
+
+
+
+
+	<a href="/prosjekter/Flyttutavbyen"><div class="prosjekt2">
+
+		<div class="tekstwrapper">
+
+						<h4>Block Watne</h4>
+
+						<h5>Flytt ut av byen</h5>
+			
+				</div>
+			<div class="sortoverlay"></div>
+			<img id="thumb" src="../img/BlockWatne/flyttutavbyen.jpg" alt="Block Watne flytt ut av byen">
+
+		</a>	
+	</div>
+
+
+
+
+
+
+
+
+
+	<a href="/prosjekter/Millum"><div class="prosjekt2">
+
+		<div class="tekstwrapper">
+
+			
+						<h4>Millum</h4>
+					
+						<h5>Filmer om Millum</h5>
+					
+			
+				</div>
+			<div class="sortoverlay"></div>
+				<img id="thumb" src="../img/Millum/Millum_forsidebilde_medium.jpg"" alt="Bilde fra Millum">
+			
+	</a>
+	</div>
+
+
+
+
+
+
+
+
+
+	<a href="/prosjekter/Sparebank1"><div class="prosjekt2">
+
+		<div class="tekstwrapper">
+
+						<h4>Sparebank1</h4>
+				
+						<h5>Film & aktivitet</h5>
+			
+		</div>
+
+			<div class="sortoverlay"></div>
+			<img id="thumb" src="../img/Sparebank1/Sparebank1.jpg"" alt="Bilde fra Sparebank 1">
+		</a>
+	</div>
+
+
+
+
+
+
+
+
+			<a href="/prosjekter/obos"><div class="prosjekt2">
+
+		<div class="tekstwrapper">
+
+						<h4>OBOS</h4>
+				
+						<h5>Forkjøpsrett</h5>
+			
+				</div>
+			<div class="sortoverlay"></div>
+				<img id="thumb" src="../img/Obos/Obos-thumb.png" alt="Bilde fra OBOS">
+	</a>
+	</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+		<a href="/prosjekter/Nordea"><div class="prosjekt2">
+
+		<div class="tekstwrapper">
+
+						<h4>Nordea</h4>
+				
+						<h5>Nordea News</h5>
+			
+				</div>
+			<div class="sortoverlay"></div>
+				<img id="thumb" src="../img/Nordea/Nordea_forsidebilde.jpg" alt="Bilde fra Nordea">
+	</a>
+	</div>
+
+
+
+
+
+
+
+
+
+	<a href="/prosjekter/Bouvetfilmen.php"><div class="prosjekt2">
+
+		<div class="tekstwrapper">
+
+						<h4>Bouvet</h4>
+				
+						<h5>Film 2018</h5>
+			
+				</div>
+			<div class="sortoverlay"></div>
+			<img id="thumb" src="../img/Bouvetfilmen/bouvetfilmen-thumb.jpg" alt="Bilde fra Colosseum kino">
+	</a>
+	</div>
+
+
+
+
+
+
+
+	<a href="/prosjekter/Kreftforeningen"><div class="prosjekt2">
+
+		<div class="tekstwrapper">
+
+						<h4>Kreftforeningen</h4>
+
+						<h5>Stjerner gir håp</h5>
+			
+				</div>
+			<div class="sortoverlay"></div>
+			<img id="thumb" src="../img/Kreftforeningen/Kreftforeningen.jpg" alt="Bilde fra Kreftforeningen">
+	</a>
+	</div>
+
+
+
+
+
+
+
+
+
+
+	<a href="/prosjekter/Studentarbeider"><div class="prosjekt2">
+
+		<div class="tekstwrapper">
+
+						<h4>Studentarbeider</h4>
+				
+						<h5>Diverse</h5>
+			
+				</div>
+			<div class="sortoverlay"></div>
+			<img id="thumb" src="../img/Studentarbeider/Ikea_stolleken_forsidebilde_small.jpg" alt="Bilde fra Studentarbeider">
+		</a>
+	</div>
+
+
+
+
+
+
+
+
+	<a href="/prosjekter/Photoshoping"><div class="prosjekt2">
+
+		<div class="tekstwrapper">
+
+						<h4>Photoshoping</h4>
+				
+						<h5>Ekstra</h5>
+			
+				</div>
+			<div class="sortoverlay"></div>
+				<img id="thumb" src="../img/Photoshoping/Nike_Halloween_newedition_retusj_8.jpg" alt="Bilde fra Photoshopping">
+		
+		</a>
+	</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="bunnen">
+		
+		<div class="ikoner2">
+
+			<a href="https://www.facebook.com/ole.kveseth" target="_blank" 
+			div class="fbcircle">
+			<img class="facebook" src="img/Main/Facebook.svg"/></a>
+		
+		
+			<a href="https://www.linkedin.com/in/ole-hallvard-kveseth-97706744/" target="_blank" 
+			div class="licircle">
+			<img class="linkdin" src="img/Main/linkedin-icon.png"/></a>
+
+			<div class="tiltoppenikon" a href="#top" onclick="smoothScroll('top')">
+			<img class="pil" src="img/Main/piloppover.svg"/> 
+			</a></div>
+
+		</div>
+
+	
+		
+	<div class="bunntekst"><h3>Ole Hallvard Kveseth<h3></div>
+
+
+</div>
+
+
+
+
+</div>
+</div>
+
+
+
+
+<div class="bakgrunnsfargen"></div>
+
+
+</body>
+</html>

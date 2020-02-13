f7af609a7a1cafae270a3601b37e3436c116d449
OleHallvard <ole_hallvard@hotmail.com>
2020-02-13 23:45:50 +0100

    animert film i bg

--- prosjekter/imdi.php
+++ prosjekter/imdi.php
@@ -3,70 +3,40 @@
 <html>
 
 <head>
-  <meta charset="UTF-8">
-  <title>OH I IMDi</title>
-  <link rel="stylesheet" href="/css/deltstyle.css">
-   <link rel="stylesheet" href="/css/deltstyle_prosjekt.css">
+    <meta charset="UTF-8">
+    <title>OH I IMDi</title>
+    <link rel="stylesheet" href="/css/deltstyle.css">
+    <link rel="stylesheet" href="/css/deltstyle_prosjekt.css">
 
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:100,200,300,300i,400,500,600,700" rel="stylesheet">
-  <script src="js/smoothScroll.js"></script>
-<script src="https://unpkg.com/scrollreveal/dist/scrollreveal.min.js"></script>
-<meta name="viewport" content="width=device-width">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:100,200,300,300i,400,500,600,700" rel="stylesheet">
+    <script src="js/smoothScroll.js"></script>
+    <script src="https://unpkg.com/scrollreveal/dist/scrollreveal.min.js"></script>
+    <meta name="viewport" content="width=device-width">
 </head>
 
 
 
 <style>
-::-moz-selection { /* Code for Firefox */
+::-moz-selection {
+    /* Code for Firefox */
     color: #eee;
     background: #1b14ac;
 }
 
 ::selection {
-    color: #eee;	
-    background:  #1b14ac;
+    color: #eee;
+    background: #1b14ac;
 }
-
-
 </style>
 
 
 <body>
 
-<?php include("../includes/meny.php"); ?>
+    <?php include("../includes/meny.php"); ?>
 
 
 
-<div class="tiltoppen"><a id="top"></a></div>
-
-
-
-
-
-
-
-
-<div class="velkommen">
-
-
-			
-
-			<img class="forsidebilde-mobil" src="../img/imdi/imdi_toppbilde.jpg" alt="IMDi">
-
-			<img class="forsidebilde-desktop-m" src="../img/imdi/imdi_toppbilde.jpg" alt="IMDi">
-
-			<img class="forsidebilde-desktop-l" src="../img/imdi/imdi_toppbilde.jpg" alt="IMDi">
-	
-		
-			<div class="tekstwrapper">
-
-					<h4 class="reveal-text">IMDi</h4>
-		
-					<h5 class="reveal-text2">Motivasjonsfilm</h5>
-			</div>
-	
-		
-	</div>
+    <div class="tiltoppen"><a id="top"></a></div>
 
 
 
@@ -75,150 +45,128 @@
 
 
 
-	
-	<div class="ikoner">
-
-
-		<div class="scroll-downs">
-  			<div class="mousey">
-    			<div class="scroller"></div>
-  			</div>
-		</div>
-		
-		<div class="scrolltekst">S<br>c<br>r<br>o<br>l<br>l</div>
-		
-			<img class="pilnedover2" src="/img/Main/pilnedover_bla.svg"/></a>
-		
-		</div>
-
-
-
-</div>
+    <div class="velkommen">
 
 
 
 
-<div class="prosjektwrapper">
+        <img class="forsidebilde-mobil" src="../img/imdi/imdi_toppbilde.jpg" alt="IMDi">
 
-<p>Motivasjonsfilm i forbindelse med intern lansering av nye systemer for søknadsbaserte tilskudd, eApply.<br><br>
-	</p>
+        <img class="forsidebilde-desktop-m" src="../img/imdi/imdi_toppbilde.jpg" alt="IMDi">
 
-	<center><p><b>Animasjonsfilm</b>
-	</p></center>
+        <img class="forsidebilde-desktop-l" src="../img/imdi/imdi_toppbilde.jpg" alt="IMDi">
 
 
-	<div class="content-film-container">
+        <div class="tekstwrapper">
+
+            <h4 class="reveal-text">IMDi</h4>
+
+            <h5 class="reveal-text2">Motivasjonsfilm</h5>
+        </div>
 
 
-  <div class="video-wrapper">
-    <div class="video-container-breddeformat"> <!-- Breddeformat -->
-      <iframe src="https://player.vimeo.com/video/302707479" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
     </div>
-  </div>
-
-
-</div>
-
-	<p>IMDi skal gjennomføre regjeringens integreringspolitikk. IMDi skal styrke kommunenes, sektormyndighetenes og andre samarbeidspartneres kompetanse på integrering og mangfold. Denne animasjonsfilmen ble laget ifb med lansering av nye datasystemer som skal forenkle prosessen både for søker og saksbehandler ved søknadsbaserte tilskudd.
-	</p>
-
-
-<!--	<img class="prosjektbilde2" src="/img/Bouvetfilmen/bouvetfilmen-colosseum.jpg"/>-->
 
 
 
 
 
-	<p><br><b><br>Andre bidragsytere:</b><br>Ole-Henrik Larssen</b>
-
-	
 
 
 
 
-<!-- Om man har stående bilde bruke denne koden under 
+    <div class="ikoner">
+
+
+        <div class="scroll-downs">
+            <div class="mousey">
+                <div class="scroller"></div>
+            </div>
+        </div>
+
+        <div class="scrolltekst">S<br>c<br>r<br>o<br>l<br>l</div>
+
+        <img class="pilnedover2" src="/img/Main/pilnedover_bla.svg" /></a>
+
+    </div>
+
+
+
+    </div>
+
+
+
+
+    <div class="prosjektwrapper">
+
+        <p>Animasjonsfilm i forbindelse med intern lansering av det nye systemet IMDinett mottak, som forenkler
+            hverdagen til mottaksansatte rundt om i landet og IMDi, og forhåpentligvis gir flyktninger et godt sted å
+            bo. Se film for å lære
+            mer :-)<br><br>
+        </p>
+
+        <center>
+            <p><b>Animasjonsfilm</b>
+            </p>
+        </center>
+
+
+        <div class="content-film-container">
+
+
+            <div class="video-wrapper">
+                <div class="video-container-breddeformat">
+                    <!-- Breddeformat -->
+                    <iframe src="https://player.vimeo.com/video/377254014" width="640" height="360" frameborder="0"
+                        webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                </div>
+            </div>
+
+
+        </div>
+        <!--
+        <p>IMDi skal gjennomføre regjeringens integreringspolitikk. IMDi skal styrke kommunenes, sektormyndighetenes og
+            andre samarbeidspartneres kompetanse på integrering og mangfold. Denne animasjonsfilmen ble laget ifb med
+            lansering av nye datasystemer som skal forenkle prosessen både for søker og saksbehandler ved søknadsbaserte
+            tilskudd.
+		</p>
+		-->
+
+
+        <!--	<img class="prosjektbilde2" src="/img/Bouvetfilmen/bouvetfilmen-colosseum.jpg"/>-->
+
+
+
+
+
+        <p><br><b><br>Andre bidragsytere:</b><br> Ole-Henrik Larssen, Tekstforfatter</b>
+
+
+
+
+
+
+            <!-- Om man har stående bilde bruke denne koden under 
 	<img class="prosjektbilde" src="img/FLYT0056_230x297_Amagasinet_nettside.jpg"/>
-	-->	
-
-
-	
+	-->
 
 
 
 
 
-<!--
+
+
+
+            <!--
 	<div class="tobilderwrapper">
 		<img class="prosjektbilde_kvadrat" src="img/BW_firkant.jpg"/>
 		<img class="prosjektbilde_kvadrat" src="img/BW_firkant.jpg"/>
 
 	</div>
-	-->	
+	-->
 
 
-</div>
-	
-
-
-
-
-
-
-
-
-
-<div class="nesteprosjektwrapper">
-
- <a href="/prosjekter/Photoshoping.php"> <div class="circle c2 revealer">
-  	<div class="container container--circles">
- 		 <div class="circle c1 revealer">
-   			<div class="circle__inner">
-     			<div class="prosjekt2 revealer">
-
-			<div class="tekstwrapper">
-				<h4 class="reveal-text3">Forrige prosjekt</h4>
-				<h6 class="reveal-text3">Ekstra</h6>
-			</div>
-			
-			<div class="sortoverlay3"></div>
-			<img id="thumb" src="../img/Photoshoping/Nike_Halloween_newedition_retusj_8.jpg" alt="Photoshopping">
-			<!--<div class="forrigeprosjektbilde"></div>-->
-	
-
-					</div>
-				</div>	
-    		</div>
-  		</div>
-  	</a>
-
-
-
-
-  <a href="/prosjekter/113appen.php"> <div class="circle c2 revealer">
-  	<div class="container container--circles">
- 		 <div class="circle c1 revealer">
-   			<div class="circle__inner">
-     			<div class="prosjekt2 revealer">
-
-			<div class="tekstwrapper">
-				<h4 class="reveal-text3">Neste prosjekt</h4>
-				<h6 class="reveal-text3">SNLA</h6>
-			
-			</div>
-			
-			<div class="sortoverlay3"></div>
-			<img id="thumb" src="../img/Snla/snla_thumb_love.png" alt="113appen">
-		
-		
-
-				</div>
-			</div>	
-    	</div>
-  	</div>
- 	</a>
-</div>
-</div>
-</div>
+    </div>
 
 
 
@@ -229,9 +177,61 @@
 
 
 
+    <div class="nesteprosjektwrapper">
+
+        <a href="/prosjekter/Photoshoping.php">
+            <div class="circle c2 revealer">
+                <div class="container container--circles">
+                    <div class="circle c1 revealer">
+                        <div class="circle__inner">
+                            <div class="prosjekt2 revealer">
+
+                                <div class="tekstwrapper">
+                                    <h4 class="reveal-text3">Forrige prosjekt</h4>
+                                    <h6 class="reveal-text3">Ekstra</h6>
+                                </div>
+
+                                <div class="sortoverlay3"></div>
+                                <img id="thumb" src="../img/Photoshoping/Nike_Halloween_newedition_retusj_8.jpg"
+                                    alt="Photoshopping">
+                                <!--<div class="forrigeprosjektbilde"></div>-->
 
 
-<?php include("../includes/prosjektfooter.php"); ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </a>
+
+
+
+
+        <a href="/prosjekter/113appen.php">
+            <div class="circle c2 revealer">
+                <div class="container container--circles">
+                    <div class="circle c1 revealer">
+                        <div class="circle__inner">
+                            <div class="prosjekt2 revealer">
+
+                                <div class="tekstwrapper">
+                                    <h4 class="reveal-text3">Neste prosjekt</h4>
+                                    <h6 class="reveal-text3">SNLA</h6>
+
+                                </div>
+
+                                <div class="sortoverlay3"></div>
+                                <img id="thumb" src="../img/Snla/snla_thumb_love.png" alt="113appen">
+
+
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </a>
+    </div>
+    </div>
+    </div>
 
 
 
@@ -240,13 +240,26 @@
 
 
 
-  <script src='https://cdnjs.cloudflare.com/ajax/libs/classlist/2014.01.31/classList.min.js'></script>
 
-   <script src="js/index.js"></script>
+
+
+
+    <?php include("../includes/prosjektfooter.php"); ?>
+
+
+
+
+
+
+
+
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/classlist/2014.01.31/classList.min.js'></script>
+
+    <script src="js/index.js"></script>
 
 
 
 
 </body>
-</html>
 
+</html>

bc0bb4e56d3b65fd7c7c3f0ea281b883bda2a736
OleHallvard <ole_hallvard@hotmail.com>
2020-02-27 16:49:11 +0100

    byttet navn på credit på  bgp

--- prosjekter/Bouvetgrandprix.php
+++ prosjekter/Bouvetgrandprix.php
@@ -3,230 +3,175 @@
 <html>
 
 <head>
-  <meta charset="UTF-8">
-  <title>OH I Bouvet Grand Prix</title>
-  <link rel="stylesheet" href="/css/deltstyle.css">
-   <link rel="stylesheet" href="/css/deltstyle_prosjekt.css">
+    <meta charset="UTF-8">
+    <title>OH I Bouvet Grand Prix</title>
+    <link rel="stylesheet" href="/css/deltstyle.css">
+    <link rel="stylesheet" href="/css/deltstyle_prosjekt.css">
 
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:100,200,300,300i,400,500,600,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:100,200,300,300i,400,500,600,700" rel="stylesheet">
 
-<script src="https://unpkg.com/scrollreveal/dist/scrollreveal.min.js"></script>
-<meta name="viewport" content="width=device-width">
+    <script src="https://unpkg.com/scrollreveal/dist/scrollreveal.min.js"></script>
+    <meta name="viewport" content="width=device-width">
 </head>
 
 
 
 <style>
-::-moz-selection { /* Code for Firefox */
+::-moz-selection {
+    /* Code for Firefox */
     color: #eee;
     background: #1b14ac;
 }
 
 ::selection {
     color: #eee;
-    background:  #1b14ac;
+    background: #1b14ac;
 }
-
-
 </style>
 
 
 <body>
 
-<?php include("../includes/meny.php"); ?>
+    <?php include("../includes/meny.php"); ?>
 
 
 
-<div class="tiltoppen"><a id="top"></a></div>
+    <div class="tiltoppen"><a id="top"></a></div>
 
 
-<div class="velkommen">
-
-
-			
-			
-
-			<img class="forsidebilde-mobil" src="../img/Bouvetgrandprix/BGP_Toppbilde_mobil.jpg" alt="Bouvet Grand Prix forsidebilde">
-
-			<img class="forsidebilde-desktop-m" src="../img/Bouvetgrandprix/BGP_Toppbilde.jpg" alt="Bouvet Grand Prix forsidebilde">
-
-			<img class="forsidebilde-desktop-l" src="../img/Bouvetgrandprix/BGP_Toppbilde.jpg" alt="Bouvet Grand Prix forsidebilde">
-
-	
-		
-			<div class="tekstwrapper">
-
-					<h4 class="reveal-text">Bouvet</h4>
-		
-					<h5 class="reveal-text2">Bouvet Grand Prix</h5>
-			</div>
-	
-		
-	</div>
+    <div class="velkommen">
 
 
 
 
 
-	
-	<div class="ikoner">
+        <img class="forsidebilde-mobil" src="../img/Bouvetgrandprix/BGP_Toppbilde_mobil.jpg"
+            alt="Bouvet Grand Prix forsidebilde">
 
+        <img class="forsidebilde-desktop-m" src="../img/Bouvetgrandprix/BGP_Toppbilde.jpg"
+            alt="Bouvet Grand Prix forsidebilde">
 
-		<div class="scroll-downs">
-  			<div class="mousey">
-    			<div class="scroller"></div>
-  			</div>
-		</div>
-		
-		<div class="scrolltekst">S<br>c<br>r<br>o<br>l<br>l</div>
-		
-			<img class="pilnedover2" src="/img/Main/pilnedover_bla.svg"/></a>
-		
-		</div>
+        <img class="forsidebilde-desktop-l" src="../img/Bouvetgrandprix/BGP_Toppbilde.jpg"
+            alt="Bouvet Grand Prix forsidebilde">
 
 
 
-</div>
+        <div class="tekstwrapper">
+
+            <h4 class="reveal-text">Bouvet</h4>
+
+            <h5 class="reveal-text2">Bouvet Grand Prix</h5>
+        </div>
 
 
-
-
-<div class="prosjektwrapper">
-
-<p>Da Bouvet i Oslo flyttet ved nyttår 2017, ga ikke det bare behov for å fortelle
-om vår nye adresse, det ga også en anledning til å fortelle noe om oss selv
-– på en avhengighetsskapende måte.
-	</p>
-
-
-
-
-	<div class="content-film-container">
-
-
-  <div class="video-wrapper">
-    <div class="video-container-breddeformat"> <!-- Breddeformat -->
-      <iframe src="https://player.vimeo.com/video/225247842" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
     </div>
-  </div>
-
-
-</div>
-
-	<p>Det gjelder å utnytte de muligheter som byr seg, og vi valgte derfor også å kombinere ”flyttemeldingen” med en leken invitasjon til den aller første kundefesten i våre nye
-lokaler. I praksis betydde dette en e-post med en festinvitasjon – og med en mulighet til å råkjøre veien fra våre gamle lokaler til de nye i et egenutviklet bilspill. Spillet var basert på basert på Google/MapBox veivisningstjeneste, og kom du høyt nok på
-high score-listen vanket det også premie på festen som beste sjåfør. Se hvor raskt du selv kan kjøre til kontoret vårt! Spill <a href="http://grandprix.bouvet.no" target="_blank">her</a> eller skriv inn grandprix.bouvet.no</p>
 
 
 
 
-	<img class="prosjektbilde2" src="/img/Bouvetgrandprix/BGP_iPad_closeup_2.jpg"/>
-
-	<img class="prosjektbilde2" src="/img/Bouvetgrandprix/BGP_iPad_closeup_.jpg"/>
-
-	<img class="prosjektbilde2" src="/img/Bouvetgrandprix/BGP_Macbooksscreen_english.jpg"/>
-
-	<img class="prosjektbilde2" src="/img/Bouvetgrandprix/BGPiphonemapenglish.jpg"/>
-
-	<img class="prosjektbilde2" src="/img/Bouvetgrandprix/BGP_Poster_facebook.jpg"/>
-
-	<img class="prosjektbilde2" src="/img/Bouvetgrandprix/BGP_bottle.jpg"/>
 
 
-<p><br><b>Andre bidragsytere:</b><br><b>Utvkler</b>: Zulfeekar "Zulu" Cheriyampurath
-<br><b>Utvikler:</b> Kristian Wille</p>
+    <div class="ikoner">
 
 
-	
+        <div class="scroll-downs">
+            <div class="mousey">
+                <div class="scroller"></div>
+            </div>
+        </div>
+
+        <div class="scrolltekst">S<br>c<br>r<br>o<br>l<br>l</div>
+
+        <img class="pilnedover2" src="/img/Main/pilnedover_bla.svg" /></a>
+
+    </div>
 
 
 
-<!-- Om man har stående bilde bruke denne koden under 
+    </div>
+
+
+
+
+    <div class="prosjektwrapper">
+
+        <p>Da Bouvet i Oslo flyttet ved nyttår 2017, ga ikke det bare behov for å fortelle
+            om vår nye adresse, det ga også en anledning til å fortelle noe om oss selv
+            – på en avhengighetsskapende måte.
+        </p>
+
+
+
+
+        <div class="content-film-container">
+
+
+            <div class="video-wrapper">
+                <div class="video-container-breddeformat">
+                    <!-- Breddeformat -->
+                    <iframe src="https://player.vimeo.com/video/225247842" width="640" height="360" frameborder="0"
+                        webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                </div>
+            </div>
+
+
+        </div>
+
+        <p>Det gjelder å utnytte de muligheter som byr seg, og vi valgte derfor også å kombinere ”flyttemeldingen” med
+            en leken invitasjon til den aller første kundefesten i våre nye
+            lokaler. I praksis betydde dette en e-post med en festinvitasjon – og med en mulighet til å råkjøre veien
+            fra våre gamle lokaler til de nye i et egenutviklet bilspill. Spillet var basert på basert på Google/MapBox
+            veivisningstjeneste, og kom du høyt nok på
+            high score-listen vanket det også premie på festen som beste sjåfør. Se hvor raskt du selv kan kjøre til
+            kontoret vårt! Spill <a href="http://grandprix.bouvet.no" target="_blank">her</a> eller skriv inn
+            grandprix.bouvet.no</p>
+
+
+
+
+        <img class="prosjektbilde2" src="/img/Bouvetgrandprix/BGP_iPad_closeup_2.jpg" />
+
+        <img class="prosjektbilde2" src="/img/Bouvetgrandprix/BGP_iPad_closeup_.jpg" />
+
+        <img class="prosjektbilde2" src="/img/Bouvetgrandprix/BGP_Macbooksscreen_english.jpg" />
+
+        <img class="prosjektbilde2" src="/img/Bouvetgrandprix/BGPiphonemapenglish.jpg" />
+
+        <img class="prosjektbilde2" src="/img/Bouvetgrandprix/BGP_Poster_facebook.jpg" />
+
+        <img class="prosjektbilde2" src="/img/Bouvetgrandprix/BGP_bottle.jpg" />
+
+
+        <p><br><b>Andre bidragsytere:</b><br><b>Tech lead/utvikler</b>: Kristian Wille
+            <br><b>Utvikler:</b> Zulfeekar "Zulu" Cheriyampurath</p>
+
+
+
+
+
+
+
+        <!-- Om man har stående bilde bruke denne koden under 
 	<img class="prosjektbilde" src="img/FLYT0056_230x297_Amagasinet_nettside.jpg"/>
-	-->	
-
-
-	
+	-->
 
 
 
 
 
-<!-- Om man ønsker to kvadratiske bilder ved siden av hverandre 
+
+
+
+        <!-- Om man ønsker to kvadratiske bilder ved siden av hverandre 
 	<div class="tobilderwrapper">
 		<img class="prosjektbilde_kvadrat" src="img/BW_firkant.jpg"/>
 		<img class="prosjektbilde_kvadrat" src="img/BW_firkant.jpg"/>
 
 	</div>
-	-->	
+	-->
 
 
-</div>
-	
-
-
-
-
-
-
-
-
-
-<div class="nesteprosjektwrapper">
-
-
-
- <a href="/prosjekter/Smartidrettsmat.php"> <div class="circle c2 revealer">
-  	<div class="container container--circles">
- 		 <div class="circle c1 revealer">
-   			<div class="circle__inner">
-     			<div class="prosjekt1 revealer">
-
-			<div class="tekstwrapper">
-				<h4 class="reveal-text3">Forrige prosjekt</h4>
-				<h6 class="reveal-text3">Tine</h6>
-			</div>
-			
-			<div class="sortoverlay3"></div>
-			<img id="thumb" src="../img/Smartidrettsmat/Tine_SI_Forsidebilde_Thumb.jpg" alt="Tine bilde">
-
-			
-	
-
-					</div>
-				</div>	
-    		</div>
-  		</div>
-  	</a>
-
-
-
-
-  <a href="/prosjekter/Ruter.php"> <div class="circle c2 revealer">
-  	<div class="container container--circles">
- 		 <div class="circle c1 revealer">
-   			<div class="circle__inner">
-     			<div class="prosjekt2 revealer">
-
-			<div class="tekstwrapper">
-				<h4 class="reveal-text3">Neste prosjekt</h4>
-				<h6 class="reveal-text3">Ruter</h6>
-			
-			</div>
-			
-			<div class="sortoverlay3"></div>
-			<img id="thumb" src="../img/Ruter/Ruter2.jpg" alt="Norwegianbilde">
-		
-		
-
-				</div>
-			</div>	
-    	</div>
-  	</div>
- 	</a>
-</div>
-</div>
-</div>
+    </div>
 
 
 
@@ -237,23 +182,91 @@ high score-listen vanket det også premie på festen som beste sjåfør. Se hvor
 
 
 
-
-<?php include("../includes/prosjektfooter.php"); ?>
-
+    <div class="nesteprosjektwrapper">
 
 
 
+        <a href="/prosjekter/Smartidrettsmat.php">
+            <div class="circle c2 revealer">
+                <div class="container container--circles">
+                    <div class="circle c1 revealer">
+                        <div class="circle__inner">
+                            <div class="prosjekt1 revealer">
+
+                                <div class="tekstwrapper">
+                                    <h4 class="reveal-text3">Forrige prosjekt</h4>
+                                    <h6 class="reveal-text3">Tine</h6>
+                                </div>
+
+                                <div class="sortoverlay3"></div>
+                                <img id="thumb" src="../img/Smartidrettsmat/Tine_SI_Forsidebilde_Thumb.jpg"
+                                    alt="Tine bilde">
 
 
 
 
-  <script src='https://cdnjs.cloudflare.com/ajax/libs/classlist/2014.01.31/classList.min.js'></script>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </a>
 
-   <script src="/js/index.js"></script>
+
+
+
+        <a href="/prosjekter/Ruter.php">
+            <div class="circle c2 revealer">
+                <div class="container container--circles">
+                    <div class="circle c1 revealer">
+                        <div class="circle__inner">
+                            <div class="prosjekt2 revealer">
+
+                                <div class="tekstwrapper">
+                                    <h4 class="reveal-text3">Neste prosjekt</h4>
+                                    <h6 class="reveal-text3">Ruter</h6>
+
+                                </div>
+
+                                <div class="sortoverlay3"></div>
+                                <img id="thumb" src="../img/Ruter/Ruter2.jpg" alt="Norwegianbilde">
+
+
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </a>
+    </div>
+    </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+    <?php include("../includes/prosjektfooter.php"); ?>
+
+
+
+
+
+
+
+
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/classlist/2014.01.31/classList.min.js'></script>
+
+    <script src="/js/index.js"></script>
 
 
 
 
 </body>
-</html>
 
+</html>
